cd667ea36667027900775cc2e9c74c064abb6546
andrew.aka.manik <andrew.aka.manik@e2148688-cdbb-13a3-a9af-e5b7de34a784>
2013-11-19 12:20:26 +0000

    [*] switch to low latency

--- proj/arm/trunk/src/c++/audiofront/audiofront.cpp
+++ proj/arm/trunk/src/c++/audiofront/audiofront.cpp
@@ -61,8 +61,8 @@ int main(int argc, const char *argv[]) {
 		std::cout << "Sample rate: " << fs << std::endl;
 
 		PaStream *audio_stream;
-		PaStreamParameters in_stream_info =  {in_dev,  1, paInt16, Pa_GetDeviceInfo(in_dev )->defaultHighInputLatency,  NULL};
-		PaStreamParameters out_stream_info = {out_dev, 1, paInt16, Pa_GetDeviceInfo(out_dev)->defaultHighOutputLatency, NULL};
+		PaStreamParameters in_stream_info =  {in_dev,  1, paInt16, Pa_GetDeviceInfo(in_dev )->defaultLowInputLatency,  NULL};
+		PaStreamParameters out_stream_info = {out_dev, 1, paInt16, Pa_GetDeviceInfo(out_dev)->defaultLowOutputLatency, NULL};
 
 		if((pa_err=Pa_OpenStream(&audio_stream, &in_stream_info, &out_stream_info, fs, paFramesPerBufferUnspecified, paClipOff, audio_stream_callback, NULL))!=paNoError)
 			throw std::runtime_error(std::string("Pa_OpenStream error: ")+Pa_GetErrorText(pa_err));
@@ -91,8 +91,8 @@ void disp_dev_info(const char *dev_type, PaDeviceIndex dev_ind) {
 	std::cout << dev_type << " (id=" << dev_ind << "):" << std::endl;
 
 	std::cout	<< "    " << api_info->name << ": " << dev_info->name << " ("
-		<< dev_info->maxInputChannels  << " ch, " << dev_info->defaultHighInputLatency  << " delay IN | "
-		<< dev_info->maxOutputChannels << " ch, " << dev_info->defaultHighOutputLatency << " delay OUT, at "
+		<< dev_info->maxInputChannels  << " ch, " << dev_info->defaultLowInputLatency  << " delay IN | "
+		<< dev_info->maxOutputChannels << " ch, " << dev_info->defaultLowOutputLatency << " delay OUT, at "
 		<< dev_info->defaultSampleRate << " Hz)"
 		<< std::endl;
 }
