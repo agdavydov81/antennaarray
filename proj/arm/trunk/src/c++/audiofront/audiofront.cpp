0a9262677fb5a155022c23b9a246d188f320d8c0
andrew.aka.manik <andrew.aka.manik@e2148688-cdbb-13a3-a9af-e5b7de34a784>
2013-11-23 23:21:45 +0000

    [*] remove two streams and output buffering

--- proj/arm/trunk/src/c++/audiofront/audiofront.cpp
+++ proj/arm/trunk/src/c++/audiofront/audiofront.cpp
@@ -22,12 +22,14 @@ int audio_stream_callback(	const void *input, void *output,
 							PaStreamCallbackFlags statusFlags,
 							void *userData );
 
+int ch_num = 1;
+
 int main(int argc, const char *argv[]) {
 	int ret=0;
 	PaError pa_err=paNoError;
 
 	try {
-		std::cout << "AudioFront 0.0.2" << std::endl;
+		std::cout << "AudioFront 0.0.3" << std::endl;
 
 		if(argc!=1 && argc!=3 && argc!=4)
 			throw std::runtime_error("Usage: audiofront [dev_in dev_out] [fs]");
@@ -60,34 +62,22 @@ int main(int argc, const char *argv[]) {
 
 		std::cout << "Sample rate: " << fs << std::endl;
 
-		int ch_num = 2;
-		PaStream *in_stream, *out_stream;
+		PaStream *audio_stream;
 		PaStreamParameters in_stream_info =  {in_dev,  ch_num, paInt16, Pa_GetDeviceInfo(in_dev )->defaultHighInputLatency,  NULL};
 		PaStreamParameters out_stream_info = {out_dev, ch_num, paInt16, Pa_GetDeviceInfo(out_dev)->defaultHighOutputLatency, NULL};
 
-		if((pa_err=Pa_OpenStream(&out_stream, NULL, &out_stream_info, fs, paFramesPerBufferUnspecified, paClipOff, NULL, NULL))!=paNoError)
-			throw std::runtime_error(std::string("Output Pa_OpenStream error: ")+Pa_GetErrorText(pa_err));
-		if((pa_err=Pa_OpenStream(&in_stream, &in_stream_info, NULL, fs, paFramesPerBufferUnspecified, paClipOff, audio_stream_callback, out_stream))!=paNoError)
-			throw std::runtime_error(std::string("Input Pa_OpenStream error: ")+Pa_GetErrorText(pa_err));
-
-		// Output stream buffering
-		if((pa_err=Pa_StartStream(out_stream))!=paNoError)
-			throw std::runtime_error(std::string("Output Pa_StartStream error: ")+Pa_GetErrorText(pa_err));
-		std::vector<int16_t> delay_buf(ch_num*(size_t)(fs*0.5));
-		if((pa_err=Pa_WriteStream(out_stream, &delay_buf[0], delay_buf.size()/ch_num)))
-			throw std::runtime_error(std::string("Output Pa_WriteStream error: ")+Pa_GetErrorText(pa_err));
+		if((pa_err=Pa_OpenStream(&audio_stream, &in_stream_info, &out_stream_info, fs, paFramesPerBufferUnspecified, paClipOff, audio_stream_callback, NULL))!=paNoError)
+			throw std::runtime_error(std::string("Pa_OpenStream error: ")+Pa_GetErrorText(pa_err));
 
 		// Now start recording
-		if((pa_err=Pa_StartStream(in_stream))!=paNoError)
-			throw std::runtime_error(std::string("Input Pa_StartStream error: ")+Pa_GetErrorText(pa_err));
+		if((pa_err=Pa_StartStream(audio_stream))!=paNoError)
+			throw std::runtime_error(std::string("Pa_StartStream error: ")+Pa_GetErrorText(pa_err));
 
 		printf("Hit ENTER to stop program.\n");
 		getchar();
 
-		if((pa_err=Pa_CloseStream(in_stream))!=paNoError)
+		if((pa_err=Pa_CloseStream(audio_stream))!=paNoError)
 			throw std::runtime_error(std::string("Input Pa_CloseStream error: ")+Pa_GetErrorText(pa_err));
-		if((pa_err=Pa_CloseStream(out_stream))!=paNoError)
-			throw std::runtime_error(std::string("Output Pa_CloseStream error: ")+Pa_GetErrorText(pa_err));
 	}
 	catch(const std::exception & err) {
 		std::cerr << err.what() << std::endl;
@@ -116,10 +106,10 @@ int audio_stream_callback(	const void *input, void *output,
 							PaStreamCallbackFlags statusFlags,
 							void *userData ) {
 
-	PaStream *out_stream = (PaStream *)userData;
-
 	if(input)
-		Pa_WriteStream(out_stream, input, frameCount);
+		memmove(output, input, ch_num*frameCount*sizeof(int16_t));
+	else
+		memset(output, 0, ch_num*frameCount*sizeof(int16_t));
 
 	return paContinue;
 }
