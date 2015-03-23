e4b2ba8909c7fbe0f08b3d40ecf5d2f3a240349e
agdavydov81 <agdavydov81@gmail.com>
2015-03-23 14:21:17 +0300

    [+] Auxiliary tools

---
+++ slsauto/thirdpart/auxiliary/lab_read.m
@@ -0,0 +1,19 @@
+function lab_info = lab_read(lab_path)
+%  lab_info = lab_read(lab_path)
+
+	% from http://stackoverflow.com/questions/6863147/matlab-how-to-display-utf-8-encoded-text-read-from-file
+	fh = fopen(lab_path, 'rb');
+	lab_info = fread(fh, '*uint8')';             %'# read bytes
+	fclose(fh);
+
+	%# decode as unicode string
+	lab_info = native2unicode(lab_info,'UTF-8');
+
+	% parse
+	lab_info = regexp(lab_info, '(\d+) (\d+) ?([^\r\n]*)', 'tokens');
+	lab_info = cellfun(@(x) struct(	'begin',str2double(x{1})/10000000, ...
+									'end',	str2double(x{2})/10000000, ...
+									'raw_begin',x{1}, ...
+									'raw_end',  x{2}, ...
+									'string',x{3}), lab_info(:));
+end
---
+++ slsauto/thirdpart/auxiliary/lab_write.m
@@ -0,0 +1,8 @@
+function lab_write(lab_info, lab_path)
+% lab_write(lab_path, lab_info)
+
+	% from http://stackoverflow.com/questions/12415767/write-unicode-strings-to-a-file-in-matlab
+	fh = fopen(lab_path, 'w');
+	arrayfun(@(x) fprintf(fh,'%s %s ',x.raw_begin,x.raw_end)+fwrite(fh,unicode2native(x.string,'UTF-8'),'uint8')+fprintf(fh,'\n'), lab_info);
+	fclose(fh);
+end
