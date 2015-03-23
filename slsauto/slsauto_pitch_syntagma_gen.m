7187956c9c3d06729219c435e7ea45a761973f47
agdavydov81 <agdavydov81@gmail.com>
2015-03-23 19:15:17 +0300

    [+] автоматизированная сегментация на синтагмы

--- slsauto/slsauto_pitch_syntagma_gen.m
+++ slsauto/slsauto_pitch_syntagma_gen.m
@@ -10,27 +10,4 @@ function slsauto_pitch_syntagma_gen(snd_pathname, lab_pathname, pitch_pathname, 
 	lab_pos = mean(unvoc_begend(ind,:),2);
 	lab_info = struct('begin',num2cell(lab_pos), 'end',num2cell(lab_pos), 'string',repmat({'syntagma'},size(lab_pos)));
 	lab_write(lab_info, lab_pathname);
-
-%{
-	lab_data = lab_read();
-
-	syntagmas_ind = false(size(unvoc_pos));
-	for li = 1:numel(lab_data)
-		syntagmas_ind( find(unvoc_pos<=lab_data(li).begin,1,'last') ) = true;
-		syntagmas_ind( find(unvoc_pos<=lab_data(li).end,1,'last') ) = true;
-	end
-	syntagmas_ind(end) = [];
-
-	unvoc_dt = diff(unvoc_pos);
-	syntagmas_dt = unvoc_dt(syntagmas_ind);
-	unvoc_dt(syntagmas_ind) = [];
-
-	figure('NumberTitle','off', 'Name',['Syntagma: ' snd_name snd_ext], 'Units','normalized', 'Position',[0 0 1 1]);
-	[unvoc_hy,unvoc_hx] = ecdf(unvoc_dt);
-	[sntgm_hy,sntgm_hx] = ecdf(syntagmas_dt);
-	plot(unvoc_hx,1-unvoc_hy,'b.-', sntgm_hx,sntgm_hy,'r.-');
-	grid('on');
-	legend({'Unvocal pause size','Syntagma pause size'},'Location','SE');
-	legend('boxoff');
-%}
 end
