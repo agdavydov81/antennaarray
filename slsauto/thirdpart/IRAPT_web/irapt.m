bf41ce80e926f7d00f2cef6e51ea314a98b1d27b
agdavydov81 <agdavydov81@gmail.com>
2015-04-13 15:00:25 +0300

    [*] fix string mistake

--- slsauto/thirdpart/IRAPT_web/irapt.m
+++ slsauto/thirdpart/IRAPT_web/irapt.m
@@ -1,9 +1,11 @@
-function [ F0, time_marks ] = irapt(Sig, Fs, type)
+function [ F0, time_marks, Voc_value] = irapt(Sig, Fs, type)
 %IRAPT -- implementation of instanteneous RAPT algorithm.
+%  [F0, time_marks, Voc_value] = irapt(Sig, Fs, type)
 %  Sig -- input signal
 %  Fs  -- sampling frequency
 %  F0  -- f0 estimations
 %  time_marks -- corresponding time marks (in seconds)
+%  Voc_value -- vocalization degree in [0 1] range
 %  type -- 'irapt1' or 'irapt2'
 
 if(Fs~=44100)
@@ -11,12 +13,12 @@ if(Fs~=44100)
     Fs=44100;
 end
 
-[F0,~,Cfg] = irapt1(Sig');
-if (strcmpi(type,'ipart2'))
+[F0,Voc_value,Cfg] = irapt1(Sig');
+Voc_value(numel(F0)+1:end) = [];
+if (strcmpi(type,'irapt2'))
     F0 = irapt2(Cfg,Sig',F0);   %IRAPT2
 end
 
-time_marks = (0:length(F0)-1)*Cfg.step_smp/Fs;
+time_marks = (0:length(F0)-1)'*Cfg.step_smp/Fs;
 
 end
-
