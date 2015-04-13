6daa51a2c8bfa97ec2dd7e5efb1aa999ec682587
agdavydov81 <agdavydov81@gmail.com>
2015-04-13 15:02:15 +0300

    [*] fix pitch estimation

--- slsauto/slsauto_getpath.m
+++ slsauto/slsauto_getpath.m
@@ -17,10 +17,21 @@ function [file_pathname is_auto] = slsauto_getpath(cfg, filetype)
 				error('Can''t deduce pitch file name for sound file ''%s''.',cfg.snd_pathname);
 			end
 			file_pathname = fullfile(snd_path, list(end).name);
+		case 'pitch_uv'
+			list = dir([cfg.snd_pathname '.pitch_*_pitchrapt*.txt']);
+			if isempty(list)
+				file_pathname = slsauto_getpath(cfg, 'pitch');
+			else
+				[~,si] = sort({list.name});
+				list = list(si);
+				file_pathname = fullfile(snd_path, list(end).name);
+			end
 		case 'prosody'
 			file_pathname = [cfg.snd_pathname '.prosody.xml'];
 		case 'lpc'
 			file_pathname = [cfg.snd_pathname '.lpc.mat'];
+		case 'synth'
+			file_pathname = [cfg.snd_pathname '.synth.wav'];
 		otherwise
 			error('Unsupported auto type identification.');
 	end
