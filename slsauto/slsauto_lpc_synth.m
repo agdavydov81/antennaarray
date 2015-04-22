ac866bdb2f9ce4a3238cbcd5f8af9e91601a596f
agdavydov81 <agdavydov81@gmail.com>
2015-04-22 21:40:44 +0300

    [*] slsauto_lpc_synth: Добавлено использование коэффициента усиления b при синтезе. [*] slsauto_lpc_synth: Добавлено сглаживание LSF и b коэффициентов на стыках участков синтеза.

--- slsauto/slsauto_lpc_synth.m
+++ slsauto/slsauto_lpc_synth.m
@@ -30,21 +30,33 @@ function slsauto_lpc_synth(cfg, synth_t, border_type)
 		regions.finish(end+1,:) = [cur_block(end)+1 cur_syntagm(2)];
 		regions.middle = [regions.middle; [cur_block(1:end-1)+1 cur_block(2:end)]];
 	end
+	
+	% КИХ НЧ фильтр Гаусса для сглаживания треков LSF на стыках блоков синтеза
+	lsf_fs = lpc_cache.fs / (lpc_cache.lpc_lsf_ind(2)-lpc_cache.lpc_lsf_ind(1));
+	lsf_fc = 20*2/lsf_fs;
+	if lsf_fc>0.5
+		lsf_b = [];
+	else
+		lsf_b = gaussfilter(lsf_fc, 1);
+	end
 
 	%% Основной цикл синтеза речеподобных сигналов
 	synth_y = zeros(0,1);
 	while size(synth_y,1)/lpc_cache.fs < synth_t
 		%% Синтез очередной синтагмы
+		% Определение требуемой длительности
 		cur_length = interp1q(prosody.syntagm_length.cdf, prosody.syntagm_length.arg, rand()) + ...
 					 interp1q(prosody.pause_length.cdf, prosody.pause_length.arg, 0.5);
 
+		% Составление списка регионов
 		reg_list = [get_region(lpc_cache, regions.start (randi(size(regions.start, 1)),:)); ...
 					get_region(lpc_cache, regions.finish(randi(size(regions.finish,1)),:))];
 
 		while sum(arrayfun(@(x) diff(x.lpc_e_t([1 end])), reg_list)) < cur_length*0.9
 			reg_list = [reg_list(1:end-1); get_region(lpc_cache, regions.middle(randi(size(regions.middle,1)),:)); reg_list(end)];
 		end
-		
+
+		% Формирование единой шкалы времени у смежных регионов синтеза
 		time_val = 0;
 		for ri = 1:numel(reg_list)
 			reg_list(ri).lpc_lsf_t = reg_list(ri).lpc_lsf_t - reg_list(ri).lpc_e_t(1) + time_val;
@@ -52,7 +64,27 @@ function slsauto_lpc_synth(cfg, synth_t, border_type)
 			time_val = reg_list(ri).lpc_e_t(end)*2-reg_list(ri).lpc_e_t(end-1);
 		end
 
-		cur_y = lpc_synth(lpc_cache.fs, vertcat(reg_list.lpc_e), vertcat(reg_list.lpc_e_t), vertcat(reg_list.lpc_lsf), vertcat(reg_list.lpc_lsf_t));
+		% Сглаживание переходов параметров на стыках регионов синтеза
+		if ~isempty(lsf_b)
+			lsf_bn = numel(lsf_b);
+			lsf_bn2 = fix(numel(lsf_b)/2);
+			for ri = 1:numel(reg_list)-1
+				% Сглаживание LSF треков
+				cur_obs = [reg_list(ri).lpc_lsf(end-lsf_bn+2:end,:); reg_list(ri+1).lpc_lsf(1:lsf_bn-1,:)];
+				cur_obs = filter(lsf_b,1,cur_obs);
+				reg_list(ri).lpc_lsf(end-lsf_bn2+1:end,:) = cur_obs(end-lsf_bn2+1-lsf_bn2:end-lsf_bn2,:);
+				reg_list(ri+1).lpc_lsf(1:lsf_bn2,:) =       cur_obs(end-lsf_bn2+1        :end        ,:);
+
+				% Сглаживание коэффициента усиления
+				cur_obs = [reg_list(ri).lpc_b(end-lsf_bn+2:end); reg_list(ri+1).lpc_b(1:lsf_bn-1)];
+				cur_obs = filter(lsf_b,1,cur_obs);
+				reg_list(ri).lpc_b(end-lsf_bn2+1:end) = cur_obs(end-lsf_bn2+1-lsf_bn2:end-lsf_bn2);
+				reg_list(ri+1).lpc_b(1:lsf_bn2) =       cur_obs(end-lsf_bn2+1        :end        );
+			end
+		end
+
+		% Синтез
+		cur_y = lpc_synth(lpc_cache.fs, vertcat(reg_list.lpc_e), vertcat(reg_list.lpc_e_t), vertcat(reg_list.lpc_lsf), vertcat(reg_list.lpc_lsf_t), vertcat(reg_list.lpc_b));
 
 		synth_y = [synth_y; cur_y]; %#ok<AGROW>
 	end
@@ -63,14 +95,22 @@ function slsauto_lpc_synth(cfg, synth_t, border_type)
 %	wavwrite(y, lpc_cache.fs, 'tmp.wav');
 end
 
+function y = filter_fir_nodelay(b,x)
+	n = numel(b);
+	n2 = fix(n/2);
+	y = filter(b,1,[repmat(x(1,:),n-1,1); x; repmat(x(end,:),n2,1)]);
+	y(1:n2+n-1,:) = [];
+end
+
 function cur_reg = get_region(lpc_cache, ind)
 	cur_reg = struct('lpc_e',lpc_cache.lpc_e(ind(1):ind(2)), 'lpc_e_t',lpc_cache.lpc_e_t(ind(1):ind(2)));
 	ind_lsf = cur_reg.lpc_e_t(1)<=lpc_cache.lpc_lsf_t & lpc_cache.lpc_lsf_t<=cur_reg.lpc_e_t(end);
 	cur_reg.lpc_lsf = lpc_cache.lpc_lsf(ind_lsf,:);
 	cur_reg.lpc_lsf_t = lpc_cache.lpc_lsf_t(ind_lsf,:);
+	cur_reg.lpc_b = lpc_cache.lpc_b(ind_lsf);
 end
 
-function y = lpc_synth(fs, e, e_t, lsf, lsf_t)
+function y = lpc_synth(fs, e, e_t, lsf, lsf_t, b)
 	resample_factor = ceil(1/(fs*min(diff(e_t)))+0.5);
 
 	% Передискретизация ошибки предсказания вверх, что бы избежать
@@ -87,13 +127,14 @@ function y = lpc_synth(fs, e, e_t, lsf, lsf_t)
 	fs = fs/resample_factor;
 
 	lsf = interp1q([0; lsf_t; max(lsf_t(end),(size(e,1)-1)/fs)], [lsf(1,:); lsf; lsf(end,:)], (0:size(e,1)-1)'/fs);
+	b   = interp1q([0; lsf_t; max(lsf_t(end),(size(e,1)-1)/fs)], [b(1,:);   b;   b(end,:)  ], (0:size(e,1)-1)'/fs);
 
 	a = lsf2poly_even(lsf);
 
 	y = zeros(size(e));
-	[~,Z] = filter(1,a(1,:),0);
+	[~,Z] = filter(b(1),a(1,:),0);
 	for ii = 1:size(e,1)
-		[y(ii),Z] = filter(1,a(ii,:),e(ii),Z);
+		[y(ii),Z] = filter(b(ii),a(ii,:),e(ii),Z);
 	end
 end
 
@@ -126,4 +167,26 @@ function a = lsf2poly_even(f)
 	for i = 2:m
 		a(:,i+1) = 0.5 * (p(:,i) + p(:,i+1) + q(:,i+1) - q(:,i));
 	end
+end
+
+function b = gaussfilter(w_c, order_odd_even)
+	if nargin<2
+		order_odd_even = 0;
+	end
+
+	N = (0.9653582771365725*w_c + 2.258890242724331)./w_c;
+	switch order_odd_even
+		case 0
+			N = round(N);
+		case 1
+			N = round((N-1)/2)*2+1;
+		case 2
+			N = round(N/2)*2;
+		otherwise
+			error('Unknown order_odd_even value.');
+	end
+		
+	x = linspace(-3,3,N);
+	b = exp(-0.5*x.^2); % ./(N.*(-0.1762119423292104*w_c + 0.4166173757357399));
+	b = b/sum(b);
 end
