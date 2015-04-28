522237573000ecc2d36a736d3395a12193180a43
agdavydov81 <agdavydov81@gmail.com>
2015-04-28 21:15:56 +0300

    [*] fixes

--- slsauto/auxiliary/filter_fir_nodelay.m
+++ slsauto/auxiliary/filter_fir_nodelay.m
@@ -3,7 +3,8 @@ function y = filter_fir_nodelay(b, x, delay)
 	if nargin<3
 		delay = fix(n/2);
 
-		if ~isequal(b(1:delay), b(end:-1:end-delay+1))
+		b_ = abs(b);
+		if max( (b_(1:delay) - b_(end:-1:end-delay+1))./b_(1:delay) ) > 1e-5
 			error('This function supports automatic group delay only for symmetric filters. Try filtfilt.');
 		end
 	end
--- slsauto/auxiliary/gaussfilter.m
+++ slsauto/auxiliary/gaussfilter.m
@@ -1,6 +1,6 @@
 function b = gaussfilter(w_c, order_odd_even)
 	if nargin<2
-		order_odd_even = 0;
+		order_odd_even = 1;
 	end
 
 	N = (0.9653582771365725*w_c + 2.258890242724331)./w_c;
@@ -14,8 +14,10 @@ function b = gaussfilter(w_c, order_odd_even)
 		otherwise
 			error('Unknown order_odd_even value.');
 	end
-		
-	x = linspace(-3,3,N);
+
+	N2 = (N-1)/2;
+	x = ((0:fix(N2))-N2)*3/N2;
 	b = exp(-0.5*x.^2); % ./(N.*(-0.1762119423292104*w_c + 0.4166173757357399));
+	b = [b b(ceil(N2):-1:1)];
 	b = b/sum(b);
 end
