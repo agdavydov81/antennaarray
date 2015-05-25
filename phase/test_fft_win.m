828edc1df9752d240d188ffb0bf80b18acda4366
agdavydov81 <agdavydov81@gmail.com>
2015-05-26 02:17:40 +0300

    [*] another test commit

--- phase/test_fft_win.m
+++ phase/test_fft_win.m
@@ -16,7 +16,6 @@ function test_fft_win
 	end
 end
 
-
 function res = win_analyse(win_name, cfg)
 	win_func = str2func(win_name);
 
@@ -37,4 +36,3 @@ function res = win_analyse(win_name, cfg)
 	res.w_main = w(mi);
 	res.p_main = Hc(mi);
 end
-
