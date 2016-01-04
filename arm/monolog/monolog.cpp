26813017129324122c1cb42d2d9a3a3ad5a94e20
Andrei Davydov <adavydov@deltixlab.com>
2016-01-04 20:04:28 +0300

    [*] ARM:SLS:Monolog: add program option "limit" and enhance sound stream save

--- arm/monolog/monolog.cpp
+++ arm/monolog/monolog.cpp
@@ -33,13 +33,14 @@ int main(int argc, const char *argv[]) {
 			("outsound", bpo::value<bfs::path>(), "set sound stream output file")
 			("outdevice", bpo::value<int>(), "set sound output device ID (-1 to disable)")
 			("listdevices", "output list of available sound devices")
+			("length", bpo::value<double>(), "minimum sound length in seconds (-1 to infinite)")
 			;
 
 		bpo::variables_map arg_vm;
 		bpo::store(bpo::parse_command_line(argc, argv, arg_desc), arg_vm);
 		bpo::notify(arg_vm);
 
-		if (argc == 1 || arg_vm.count("help")) {
+		if (arg_vm.count("help")) {
 			std::cout << arg_desc << std::endl;
 			return 0;
 		}
@@ -59,6 +60,7 @@ int main(int argc, const char *argv[]) {
 		auto stat_path = arg_vm.count("statpath") ? arg_vm["statpath"].as<bfs::path>() : bfs::path("det_res.txt");
 		auto tts_base = arg_vm.count("ttsbase") ? arg_vm["ttsbase"].as<bfs::path>() : bfs::path("db_bor1");
 		auto config_xml = arg_vm.count("config") ? arg_vm["config"].as<bfs::path>() : bfs::path("monolog.xml");
+		auto length_limit = arg_vm.count("length") ? arg_vm["length"].as<double>() : -1.0;
 
 		bpt::ptree pt;
 		{
@@ -89,9 +91,11 @@ int main(int argc, const char *argv[]) {
 
 			int format = SF_FORMAT_PCM_16 | SF_FORMAT_WAV;
 			if (!ext.compare(L".ogg"))
-				format = SF_FORMAT_OGG | SF_FORMAT_VORBIS;
+				format = SF_FORMAT_VORBIS | SF_FORMAT_OGG;
 			else if (!ext.compare(L".fla") || !ext.compare(L".flac"))
-				format = SF_FORMAT_FLAC | SF_FORMAT_PCM_16;
+				format = SF_FORMAT_PCM_16 | SF_FORMAT_FLAC;
+			else if (!ext.compare(L".w64"))
+				format = SF_FORMAT_PCM_16 | SF_FORMAT_W64;
 
 			out_sound = SndfileHandle(snd_path.c_str(), SFM_WRITE, format, tts.base.channels, tts.base.samplerate);
 
@@ -105,10 +109,16 @@ int main(int argc, const char *argv[]) {
 			if ((portaudio_error = Pa_OpenStream(&portaudio_stream, nullptr, &out_stream_info, tts.base.samplerate, paFramesPerBufferUnspecified, paNoFlag, (PaStreamCallback *)portaudio_stream_callback, &portaudio_userdata)) != paNoError)
 				throw std::runtime_error(std::string("Pa_OpenStream error: ") + Pa_GetErrorText(portaudio_error));
 		}
+		else
+			if (!arg_vm.count("length"))
+				length_limit = 100;
 
-		uint64_t frames_counter = 0;
+		if (!out_text.is_open() && !out_allophone.is_open() && !out_sound_lab.is_open() && out_device < 0)
+			throw std::runtime_error("No output specified.");
+
+		int64_t frames_counter = 0;
 		std::deque<CAllophoneTTS::MARK_DATA> marks;
-		for (int i = 0; i < 3; ++i) {
+		while (static_cast<double>(frames_counter)/tts.base.samplerate<length_limit || length_limit<0) {
 			std::string text = text_gen.Generate();
 			if (out_text.is_open())
 				out_text << text << std::endl;
@@ -120,12 +130,11 @@ int main(int argc, const char *argv[]) {
 				out_allophone << std::endl;
 			}
 
-
 			while (!queue.empty()) {
 				auto sound = tts.Allophones2Sound(queue, out_sound_lab.is_open() ? &marks : nullptr);
+				auto signal_frames = sound.size() / tts.base.channels;
 
 				if (out_sound_lab.is_open()) {
-					auto signal_frames = sound.size() / tts.base.channels;
 					out_sound.write(&sound[0], signal_frames);
 
 					for (size_t i = 1; i < marks.size(); ++i)
@@ -137,9 +146,9 @@ int main(int argc, const char *argv[]) {
 						out_sound_lab << (frames_counter + marks.back().position) * 10000000 / tts.base.samplerate << " " <<
 										(frames_counter + signal_frames + tts.prosody_delay) * 10000000 / tts.base.samplerate << " " <<
 										marks.back().name << std::endl;
-
-					frames_counter += signal_frames;
 				}
+
+				frames_counter += signal_frames;
 			}
 		}
 
